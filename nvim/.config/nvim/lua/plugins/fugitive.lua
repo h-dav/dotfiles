208a9b54e4f2ae9ba78ecf6097ff8a4520421e7d
hcdav <harry.cr.davidson@gmail.com>
2024-09-27 12:45:15 +0100

    When opening fugitive, split vertically.

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
     "tpope/vim-fugitive",
-    config = function ()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    end
+    keys = {
+        {"<leader>gs", function() vim.cmd("vert Git") end}
+    }
 }
