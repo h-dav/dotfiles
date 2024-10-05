db89d8e69badd8d0896a34ef31bd2ac63749c438
hcdav <harry.cr.davidson@gmail.com>
2024-10-05 14:23:08 +0100

    Add some keymaps.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -65,7 +65,7 @@ return {
                             cmp.complete()
                         end
                     end, { "i", "s" }),
-                    ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "s" }),
+                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete()),
                 }),
                 preselect = cmp.PreselectMode.None,
                 completion = { completeopt = "menu,menuone,noinsert,noselect,preview" },
@@ -108,10 +108,10 @@ return {
                     local opts = { buffer = bufnr }
 
                     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-                    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-                    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+                    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>zz", opts)
+                    vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>zz", opts)
                     vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-                    vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+                    vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>zz", opts)
                     vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
                     vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
                     vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,9 +5,9 @@ return {
     config = function()
         local telescope = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', telescope.find_files, {}) -- project file
-        vim.keymap.set('n', '<leader>pg', telescope.live_grep, {}) -- project grep
+        vim.keymap.set('n', '<leader>pg', telescope.live_grep, {})  -- project grep
         vim.keymap.set('n', '<leader>pr', telescope.registers, {})
-        vim.keymap.set('n', '<leader>po', telescope.oldfiles, {}) -- recently opened files
+        vim.keymap.set('n', '<leader>po', telescope.oldfiles, {})   -- recently opened files
     end
 
 }
